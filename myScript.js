1b4dd03e8e1359a4b8059c54395df7c2a52a695d
Marc Rahbani <marcrahbani@hotmail.fr>
2018-05-02 16:19:18 +0200

    avancement des pages

--- myScript.js
+++ myScript.js
@@ -9,3 +9,60 @@ function changeImage(element)
 
 x.setAttribute("src", v);
 }
+
+function changeImage2(element)
+{
+	var x = document.getElementById("image1");
+  var v = x.getAttribute("src");
+  if(v== "vous.png")
+   { v= "vous2.png";}
+  else
+  { v = "vous.png";}
+
+x.setAttribute("src", v);
+}
+function changeImage3(element)
+{
+	var x = document.getElementById("image2");
+  var v = x.getAttribute("src");
+  if(v== "reseau.png")
+   { v= "reseau2.png";}
+  else
+  { v = "reseau.png";}
+
+x.setAttribute("src", v);
+}
+function changeImage4(element)
+{
+	var x = document.getElementById("image3");
+  var v = x.getAttribute("src");
+  if(v== "notifications.png")
+   { v= "notifications2.png";}
+  else
+  { v = "notifications.png";}
+
+x.setAttribute("src", v);
+}
+function changeImage5(element)
+{
+	var x = document.getElementById("image4");
+  var v = x.getAttribute("src");
+  if(v== "messagerie.png")
+   { v= "messagerie2.png";}
+  else
+  { v = "messagerie.png";}
+
+x.setAttribute("src", v);
+}
+
+function changeImage6(element)
+{
+	var x = document.getElementById("image5");
+  var v = x.getAttribute("src");
+  if(v== "emplois.png")
+   { v= "emplois2.png";}
+  else
+  { v = "emplois.png";}
+
+x.setAttribute("src", v);
+}
