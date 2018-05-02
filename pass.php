5f1eb7a0b944ff81790ab2cb2595ca0d471d80c7
GREYGOOSE\Grégoire <gregoired2c@gmail.com>
2018-05-02 17:42:37 +0200

    connection bdd

--- pass.php
+++ pass.php
@@ -1,18 +1,35 @@
 <?php
-$login = isset($_POST["identifiant"]) ? $_POST["identifiant"] : "";
-$pass = isset($_POST["passw"]) ? $_POST["passw"] : "";
-//mots de passe stockés dans le serveur
+//identifier le nom de base de données
+ $database = "net4work";
+//connecter l'utilisateur dans BDD
+//votre serveur = localhost | votre login = root | votre mot de pass = ‘’ (rien)
+ $db_handle = mysqli_connect(‘localhost’, ‘root’, ‘’);
+ $db_found = mysqli_select_db($db_handle, $database);
+ //si la BDD existe, faire le traitement
+ 
+ if ($db_found) { 
 
-$connection = false;
+	$login = isset($_POST["identifiant"]) ? $_POST["identifiant"] : "";
+	$pass = isset($_POST["passw"]) ? $_POST["passw"] : "";
+	//mots de passe stockés dans le serveur
 
-if($login=="admin" && $pass="admin") {
-$connection = true;
-}
+	$connection = false;
 
-if($connection) {
-header("Location:main.html");
+	$sql = SELECT * FROM `connection` WHERE `email` = "$login" AND `motdepasse` = "$pass";
+	
+		$connection = true;
+	
+
+	if($connection) {
+		header("Location:main.html");
+	}
+	else {
+		header("Location:connexion.html");
+	}
 }
 else {
-header("Location:connexion.html");
-}
+ 	echo "Database not found";
+ }//end else
+//fermer la connection
+mysqli_close($db_handle); 
 ?>
